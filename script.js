f2f66f8e98e12a4a7a381b47da06d359b4422473
Scott Chen <96890985+skottchen@users.noreply.github.com>
2024-02-23 14:31:56 -0500

    Enable color on grid

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 const container = document.querySelector(".inner-container");
 
-for(let i = 1; i <= 16 * 16; i++){
-    const square = document.createElement("div")
-    square.classList.add("grid");
-    container.appendChild(square);
-    if(i % 16 == 0){
-        const linebreak = document.createElement("div")
-        linebreak.classList.add("break");
-        container.appendChild(linebreak);
+function createGrid() {
+    for (let i = 1; i <= 16 * 16; i++) {
+        const square = document.createElement("div")
+        square.classList.add("grid");
+        container.appendChild(square);
+        if (i % 16 == 0) {
+            const linebreak = document.createElement("div")
+            linebreak.classList.add("break");
+            container.appendChild(linebreak);
+        }
     }
 }
 
+function addColor() {
+    const squares = document.getElementsByClassName("grid");
+    for (let i = 0; i < squares.length; i++) {
+        squares[i].addEventListener("mouseover", () => {
+            squares[i].style.backgroundColor = "blue";
+        })
+    }
+}
 
+createGrid();
+addColor();
