d6f5982778b490a752b3b9a01e73d8b084463a45
Scott Chen <96890985+skottchen@users.noreply.github.com>
2024-02-24 22:10:57 -0500

    Enable random coloring of pixels

--- script.js
+++ script.js
@@ -13,14 +13,19 @@ function createGrid() {
     }
 }
 
-function addColor() {
+function enableColoring() {
     const squares = document.getElementsByClassName("grid");
     for (let i = 0; i < squares.length; i++) {
         squares[i].addEventListener("mouseover", () => {
-            squares[i].style.backgroundColor = "blue";
+            squares[i].style.backgroundColor = `rgb(${getRGBValue()} ${getRGBValue()} ${getRGBValue()})`;
         })
     }
 }
 
+function getRGBValue() {
+    //random number from 0 (inclusive) to 256 (inclusive)
+    return (Math.floor(Math.random() * 256)).toString();
+}
+
 createGrid();
-addColor();
+enableColoring();
